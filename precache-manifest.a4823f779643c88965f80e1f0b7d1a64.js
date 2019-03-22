c2fb6a76134c084fa500daf8b30278059719063b
Brian Nguyen <nguyenbrian12@yahoo.com>
2019-03-22 15:18:01 -0700

    Updates

---
+++ precache-manifest.a4823f779643c88965f80e1f0b7d1a64.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "960e33ca9bbeb1a125afafeb49e6c07b",
+    "url": "/ignIntro/static/media/OpenSans-Regular.960e33ca.woff"
+  },
+  {
+    "revision": "9db0c2b5b3a019fac1c2",
+    "url": "/ignIntro/static/css/main.7e8d5e98.chunk.css"
+  },
+  {
+    "revision": "de46e90ed1bfbb95954a",
+    "url": "/ignIntro/static/js/runtime~main.bc2f54e2.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/ignIntro/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "01538afda0aa18508625e0f42b4cd43f",
+    "url": "/ignIntro/static/media/Arapey-Regular.01538afd.eot"
+  },
+  {
+    "revision": "6449567592de2a88496630bb0041300e",
+    "url": "/ignIntro/static/media/Arapey-Regular.64495675.woff"
+  },
+  {
+    "revision": "df2380390b0897ff9786af1047eb1d76",
+    "url": "/ignIntro/static/media/Arapey-Regular.df238039.ttf"
+  },
+  {
+    "revision": "3ba377fca16a2c980a6b82d3970f39b3",
+    "url": "/ignIntro/static/media/Arapey-Regular.3ba377fc.svg"
+  },
+  {
+    "revision": "bb551feb34fe60fd58f9b5913dd05073",
+    "url": "/ignIntro/static/media/Marcellus-Regular.bb551feb.ttf"
+  },
+  {
+    "revision": "b180c799a87a4cad9108713ebe309686",
+    "url": "/ignIntro/static/media/OpenSans-Light.b180c799.eot"
+  },
+  {
+    "revision": "62bec72ed6a2ddb7dcf0f06dc961cd49",
+    "url": "/ignIntro/static/media/OpenSans-Light.62bec72e.woff"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/ignIntro/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "06a04537db9294681f93bc12bd1d9c53",
+    "url": "/ignIntro/static/media/OpenSans-Regular.06a04537.eot"
+  },
+  {
+    "revision": "9db0c2b5b3a019fac1c2",
+    "url": "/ignIntro/static/js/main.e8243986.chunk.js"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/ignIntro/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "98105ce8893a6fd0fad7d6456c961371",
+    "url": "/ignIntro/static/media/OpenSans-Light.98105ce8.svg"
+  },
+  {
+    "revision": "31a82a9bde90eec038693f26ada892ad",
+    "url": "/ignIntro/static/media/OpenSans-Semibold.31a82a9b.eot"
+  },
+  {
+    "revision": "89c38fd91344cb1871b43d05d8f5c0c1",
+    "url": "/ignIntro/static/media/OpenSans-Semibold.89c38fd9.woff"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/ignIntro/static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "33696c1691b434f58dd2887f2fa2a30f",
+    "url": "/ignIntro/static/media/OpenSans-Bold.33696c16.eot"
+  },
+  {
+    "revision": "6d79f9e950bea929171023cbf7363e33",
+    "url": "/ignIntro/static/media/OpenSans-Regular.6d79f9e9.svg"
+  },
+  {
+    "revision": "42520920c4d221faaad5777d01680e36",
+    "url": "/ignIntro/static/media/OpenSans-Bold.42520920.woff"
+  },
+  {
+    "revision": "ee6ef2ee3102efffb7b9ca538577f6d7",
+    "url": "/ignIntro/static/media/OpenSans-Semibold.ee6ef2ee.svg"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "/ignIntro/static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "fcba4b1ab656555d9014d8dc497e1691",
+    "url": "/ignIntro/static/media/OpenSans-Bold.fcba4b1a.svg"
+  },
+  {
+    "revision": "998f22d675d4641d3746ff3a3c1ef208",
+    "url": "/ignIntro/index.html"
+  }
+];
